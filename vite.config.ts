cb3ae10a35f5128700ad18d8c9e4813b7be8e76c
Talisson Avila <talisson_avila@hotmail.com>
2024-06-25 12:34:01 -0300

    build: include fields to build app

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build',  // Certifique-se de que o diretório de saída é 'build'
+  },
+  base: '/PomodoroReact/'  // Adicione isso se estiver usando um repositório do GitHub
+});
